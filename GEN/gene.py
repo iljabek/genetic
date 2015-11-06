fd022e47092523a4cbb6cb8bb581d0f3a6b835fd
Ilja Bekman <ilya.bekman@gmail.com>
2015-11-07 00:15:22 +0100

    rewritten and separated gene.py and indi.py

---
+++ GEN/gene.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+"GAH: Gene"
+
+#import os,sys
+import random
+import decimal
+
+
+def main():
+	pass
+	test_gene()
+
+class gene(object):
+	"""
+Gene. 
+	has val from max..min
+	setter truncates input outside range
+	mutate sets val from a uniform distribution
+	"""
+
+	def __init__(self, inval):
+		self._min = 0.
+		self._max = 1.
+		self.val = inval
+#		self._val = decimal.Decimal(0).quantize(decimal.Decimal('1.E-7'), rounding=decimal.ROUND_DOWN)
+	
+	@property
+	def max(self):
+		"Getter for max value"
+		return self._max
+	@max.setter
+	def max(self, inmax):
+		"max setter, must be >= min"
+		self._max = max(inmax, self.min)
+
+	@property
+	def min(self):
+		"Getter for min value"
+		return self._min
+	@min.setter
+	def min(self, inmin):
+		"min setter, must be <= max"
+		self._min = min(inmin, self.max)
+
+	@property
+	def val(self):
+		"Getter for current value"
+		return self._val
+	@val.setter
+	def val(self, inval):
+		"""
+		Setter puts inval into min.....val......max range
+		"""
+		tmpval = min( max(inval, self.min) , self.max)
+#		self._val = tmpval
+		self._val = decimal.Decimal(tmpval).quantize(decimal.Decimal('1.E-8'), rounding=decimal.ROUND_DOWN)
+		if tmpval != inval:
+			msg = "* gene input " + str(inval) + " outside range " 
+			msg += str(self.min) + ".." + str(self.max) 
+			msg += ", set to " +str(self.val)
+			print msg
+	
+	def __str__(self):
+		return str(self.val)+ " in (" +str(self.min) + ".." + str(self.max)+ ")"
+
+	def copy(self):
+		c = gene(self.val)
+		c.min = self.min
+		c.max = self.max
+		return c
+		
+	def __mul__(self , other):
+		"A*B: mean of each gene"
+		c = self.copy()
+		c.val = (self.val + other.val)/decimal.Decimal(2.)
+		return c
+	
+	def __add__(self , other):
+		c = self.copy()
+		rnd = random.random() >= 0.5
+		if rnd: c.val = self.val
+		else: c.val = other.val
+		return c
+	
+	def mutate(self): # shuffle
+		"new value within range"
+		self.val = random.uniform(self.min , self.max)
+
+	def perturbate(self): # shuffle
+		"move by small amount around the current value"
+		self.val = random.gauss( float(self.val) , (self.max-self.min)/10. )
+
+		pass
+
+
+def test_gene():
+	cog = gene(0.)
+	print cog
+	print cog.val
+	cog.val = 0.2
+	print cog
+	cog.val = 1.2
+	print cog
+	cog.val = -0.2
+	print cog
+	cog.mutate()
+	print "mutate: ", cog
+	cog.mutate()
+	print "mutate: ", cog
+	sproc = gene(0.2)
+	print "2nd:", sproc
+	print "A+B",cog+sproc
+	print "A+B",cog+sproc
+	print "A+B",cog+sproc
+	print "A*B",cog*sproc
+
+	print "pre: ", cog
+	cog.perturbate()
+	print "pert:", cog
+
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ GEN/indi.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+"GAH: Indi"
+
+#import os,sys
+#import math
+import random
+import copy
+#import numpy
+import hashlib
+
+from gene import gene
+
+
+def main():
+	pass
+	test_indi()
+
+class indi(object):
+	"""
+Individual.
+	contains genes, defining uniqe hash
+	can be iterated over genes
+	can mutate all or some genes
+	"""
+	def __init__(self,genes): #,invals=[]):
+		self._genes  = copy.deepcopy(genes) # list of genes
+		self.fitness = -1.e-12 # bad for integral
+		self._i = 0 # positoin for iter and next
+		"""
+		if len(invals) == len(genes):
+			for i in range(len(self._genes)): 
+				self._genes[i].vals = invals[i] 
+		"""
+
+	"""
+	@property
+	def genes(self):
+		"Getter for genes"
+		return self._genes
+	@genes.setter
+	def genes(self, ingenes):
+		""
+		if isinstance(ingenes, list):
+			if len(ingenes) == len(self):
+				if all( [isinstance(g, gene) for g in ingenes] ):
+					for i in range(len(self)):
+						self[i].val = ingenes[i].val
+				else:
+					for i in range(len(self)):
+						self[i].val = ingenes[i]
+		else:
+			for i in range(len(self)):
+				self[i].val = ingenes
+	"""
+
+	def __getitem__(self, item):
+		if isinstance(item, slice):
+			return indi(self._genes[item])
+		else:
+			return self._genes[item]
+
+	def __setitem__(self, item, val):
+		if isinstance(item, slice):
+			for i in range(item.start,item.stop):
+				self[i] = val[i]
+		else:
+			if isinstance(val, gene):
+				self._genes[item] = val
+			else:
+				self._genes[item].val = val
+
+	def __iter__(self):
+		if self._i >= len(self):
+			self._i = 0
+		return self
+
+	def next(self):
+		if self._i >= len(self):
+			self._i = 0
+			raise StopIteration()
+		else:
+			self._i += 1 
+			return self._genes[self._i-1] 
+	
+	def __len__(self):
+		return len(self._genes)
+
+	def __cmp__(self, other): # 
+		if self.fitness > other.fitness:
+			return 1
+		elif self.fitness < other.fitness:
+			return -1
+		else:
+			return 0
+
+	"""	
+	def __lt__(self, other): # <
+		return self.fitness < other.fitness
+	def __gt__(self, other): # >
+		return self.fitness > other.fitness
+	def __le__(self, other): # <=
+		return self.fitness <= other.fitness
+	def __ge__(self, other): # >=
+		return self.fitness >= other.fitness
+	def __eq__(self, other): # ==
+		return self.fitness == other.fitness
+	def __ne__(self, other): # !=
+		return self.fitness != other.fitness
+	"""
+
+	def getall(self):
+		return [g.val for g in self._genes]
+
+	@property
+	def hash(self):
+		"Hashing using all genes' values, makes inidis uniqe"
+		c = ";".join([str(i) for i in self.getall()])
+		cm = str( hashlib.md5(  c  ).hexdigest().encode('utf-8') )
+		icm = cm[0:7]
+#		icm = "{0:09d}".format(int(  cm[0:7]   ,16))
+		return icm
+
+	def __str__(self):
+		OUT="I'm "+self.hash+"! Fitness: " + str(self.fitness) +  "\n "
+		for g in self:
+			OUT += str(g) + " " 
+		return OUT
+
+### GA relevant functions below
+
+	def mitosis(self):
+		return copy.deepcopy(self._genes) 
+
+#	@classmethod
+#	def mitosis(cls,ind):
+#		return cls(ind._genes)
+
+#	def mutateI(self,I):
+#		self[I].mutate()
+	
+	def mutateAll(self):
+		for i in range(len(self)):
+			self[i].mutate()
+	
+	def perturbateAll(self):
+		for i in range(len(self)):
+			self[i].perturbate()
+	
+	def mutateAnyN(self,N):
+		mutees = []
+		for i in range(N):
+			#choose random gene from all
+			mutee = random.randint(0,len(self)-1)
+			#don't mutate same gene twice
+			if (mutee in mutees):
+				tries=0
+				while (mutee in mutees) or tries<len(self):
+					#rechoose until new
+					mutee = random.randint(0,len(self)-1)
+					tries+=1
+			self[mutee].mutate()
+#			self.mutateI(mutee)
+			mutees.append(mutee)
+
+	def __add__(self , other):
+		"A+B: create offspring with random genes from both parents"
+		child = indi(self.mitosis())
+		for i in range(len(self)):
+			child[i] = self[i] + other[i]
+		return child
+
+	def __mul__(self , other):
+		"A*B: create offspring with mean of each gene from both parents"
+		child = indi(self.mitosis())
+		for i in range(len(self)):
+			child[i] = self[i] * other[i]
+		return child
+
+def evalFit(ind):
+	print "overload me!"
+	fit = sum(ind.getall())
+	ind.fitness = fit
+	return fit
+
+
+def getCar():
+	car = []
+	car.append( gene(0) )
+	car.append( gene(0) )
+	car.append( gene(0) )
+	car.append( gene(0) )
+	car.append( gene(0) )
+	return car
+
+def test_indi():
+	jeep = indi(getCar())
+	print jeep[0]
+	for i in jeep:
+		print i
+	print jeep
+	for i in jeep:
+		i.mutate()
+	print jeep
+	jeep[1].mutate()
+	evalFit(jeep)
+	print jeep
+	jeep.mutateAll()
+	evalFit(jeep)
+	print jeep	
+	jeep.perturbateAll()
+	evalFit(jeep)
+	print jeep
+	jeep.mutateAnyN(2)
+	evalFit(jeep)
+	print jeep
+	jag = indi(jeep.mitosis())
+	evalFit(jag)
+	print jag	
+	jeep.mutateAll()
+	evalFit(jeep)
+	print jeep	
+	print jeep+jag
+	print jeep*jag
+	jeep[0:2] = jag[0:2]
+	print jeep	
+
+if __name__ == '__main__':
+	main()
